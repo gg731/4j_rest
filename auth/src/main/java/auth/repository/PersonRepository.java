9a781dbb41faab08d7c4f609723b073d06d00576
137799 <g1377@protonmail.com>
2020-10-21 16:10:35 +0300

    Initial commit

---
+++ auth/src/main/java/auth/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package auth.repository;
+
+import auth.domain.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+}
